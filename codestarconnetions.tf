1f2e4f829b8a1a059e82ff0737a03a7a4740aac6
Jonnathan De La Barra <justindelabarra@gmail.com>
2024-08-12 18:14:15 -0500

    primeros cambios

--- codestarconnetions.tf
+++ codestarconnetions.tf
@@ -1,3 +1,4 @@
+#Para conectarse a Bitbiucket, se debe crear un recurso de conexión de AWS CodeStar Connections con el tipo de proveedor Bitbucket.
 resource "aws_codestarconnections_connection" "example" {
   name          = "bitbucket_connection"
   provider_type = "Bitbucket"
